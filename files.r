bc88db53161a82e81b03145535829c6ec4836a3f
Roman Zhak <zhak55@ya.ru>
2018-03-19 11:11:45 +0300

    Create files.r

---
+++ files.r
@@ -0,0 +1,6 @@
+# transfer files between folders
+move <- function(from, to) {
+  todir <- dirname(to)
+  if (!isTRUE(file.info(todir)$isdir)) dir.create(todir, recursive=TRUE)
+  file.rename(from = from,  to = to)
+}
