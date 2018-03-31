62c3a616c79c534cc4c1b1b33c27684533bd700e
Roman Zhak <zhak55@ya.ru>
2018-03-31 22:18:31 +0300

    Update Environment.R

--- Advanced/Environment.R
+++ Advanced/Environment.R
@@ -1,10 +1,9 @@
 new_env <- new.env()
 new_env$isRightTriangle <- function(a,b,c) c^2 == a^2 + b^2
-new_env$isRightTriangle(3,4,5)
+new_env$isRightTriangle(3,4,5) # => TRUE
 
 isError <- tryCatch(isRightTriangle, error = function(e) e)
-inherits(isError, "error")
-# TRUE
+inherits(isError, "error") # => TRUE
 
 # the environment variable names
 ls(new_env)
