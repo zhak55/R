21875b4b12ec6f477508cf902b4b12ddc1783e86
Roman Zhak <zhak55@ya.ru>
2018-03-31 22:17:37 +0300

    Create Environment.R

---
+++ Advanced/Environment.R
@@ -0,0 +1,14 @@
+new_env <- new.env()
+new_env$isRightTriangle <- function(a,b,c) c^2 == a^2 + b^2
+new_env$isRightTriangle(3,4,5)
+
+isError <- tryCatch(isRightTriangle, error = function(e) e)
+inherits(isError, "error")
+# TRUE
+
+# the environment variable names
+ls(new_env)
+# remove variable and value
+rm(isRightTriangle, envir = new_env)
+# existence 
+exists("isRightTriangle", envir = new_env) # => FALSE
