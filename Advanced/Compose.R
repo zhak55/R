afe0a8f6bdba82b07f2befe03a08f30a0fdfa7a8
Roman Zhak <zhak55@ya.ru>
2018-04-01 20:09:56 +0300

    Update Compose.R

--- Advanced/Compose.R
+++ Advanced/Compose.R
@@ -1,5 +1,5 @@
 library('purrr')
 
 # composition of functions
-q_num <- compose(sum, unique, as.numeric)
-q_num(c('5', '5', '2'))
+q_num <- compose(sum, unique, as.numeric) # == sum(unique(as.numeric(x)))
+q_num(c('5', '5', '2')) # => 7
