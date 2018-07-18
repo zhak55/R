43b2a47ae90e0ad22593192170a4d1db6f8077f3
Roman Zhak <zhak55@ya.ru>
2018-07-18 11:58:44 +0300

    Update files.r

--- files.r
+++ files.r
@@ -4,3 +4,9 @@ move <- function(from, to) {
   if (!isTRUE(file.info(todir)$isdir)) dir.create(todir, recursive=TRUE)
   file.rename(from = from,  to = to)
 }
+
+# get only files from current dir
+files.only <- function() {
+  library('magrittr')
+  dir() %>% file.info() %>% {.[which(!.['isdir']), ]}
+}
